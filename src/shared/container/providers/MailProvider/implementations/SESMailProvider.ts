e537703e65e96d3478e023549e04d570abc09f33
Felipe de Senna <lipe.senna@hotmail.com>
2020-09-15 18:37:45 -0300

    configuração para deploy em produção

--- src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -2,7 +2,7 @@ import nodemailer, { Transporter } from 'nodemailer';
 import { injectable, inject } from 'tsyringe';
 import aws from 'aws-sdk';
 
-import mailConfig from 'config/mail';
+import mailConfig from '@config/mail';
 
 import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
 
